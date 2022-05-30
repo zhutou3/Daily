47e35e64bfe669e7fe177a6c99fabfa6c3420582
zhutou3 <85608669+zhutou3@users.noreply.github.com>
2022-05-30 23:55:44 +0800

    Update main1.py

--- main1.py
+++ main1.py
@@ -179,7 +179,7 @@ def main():
         favorites = get_favorite(i)
         for j in favorites:
             time.sleep(random.randint(1,5))
-            client_sign(i, tbs, j["id"], str(j))
+            client_sign(i, tbs, j["id"], kw)
         logger.info("完成第" + str(n) + "个用户签到")
     logger.info("所有用户签到结束")
 
